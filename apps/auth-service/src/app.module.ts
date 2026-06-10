bb98f29c1484b781215f4757c71fe13a22e9fb8c
Eduardo Garcia <edugarciasb16@gmail.com>
2026-06-10 21:33:28 +0200

    refactor: modularize auth-service architecture

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, AuthModule],
 })
 export class AppModule {}
